f0dee5581b9c06e4d9f68c3f781ac52a935a7ce9
Md.Rakibul Hasan <32368310+Rakibul66@users.noreply.github.com>
2023-05-21 01:07:30 +0600

    tap the target

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
-# swipdash
+Title: "Tap the Target"
 
-A new Flutter project.
+Objective: The objective of the game is to tap the moving target as many times as possible within a given time limit.
 
-## Getting Started
+Gameplay:
 
-This project is a starting point for a Flutter application.
+The game starts with a target positioned at a random location on the screen.
+The target moves around the screen at a constant speed, changing direction whenever it hits the screen boundaries.
+The player earns points by tapping the target.
+Each time the player taps the target, it disappears and reappears at a new random location on the screen.
+The game ends after a specified time limit (e.g., 30 seconds).
+The player's final score is displayed at the end of the game.
+Features to consider adding:
 
-A few resources to get you started if this is your first Flutter project:
+Display a timer countdown during gameplay.
+Add sound effects or visual effects when the target is tapped.
+Implement a high score system to track and display the player's best scores.
+Include different levels of difficulty, such as increasing the target speed or reducing its size over time.
+Remember to handle touch events and collision detection to detect when the player taps the target. You can use the GestureDetector widget for handling taps, and track the target's position using a timer or animation controller.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+Feel free to get creative and customize the game with your own ideas and enhancements!
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+If you have any specific questions or need further assistance with implementing the game, feel free to ask. Happy coding!
--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,165 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'dart:math';
 
 void main() {
-  runApp(SwipeGame());
+  runApp(TapTheTargetGame());
 }
 
-class SwipeGame extends StatelessWidget {
+class TapTheTargetGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Swipe Game',
+      title: 'Tap the Target',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SwipeGamePage(),
+      home: GamePage(),
     );
   }
 }
 
-class SwipeGamePage extends StatefulWidget {
+class GamePage extends StatefulWidget {
   @override
-  _SwipeGamePageState createState() => _SwipeGamePageState();
+  _GamePageState createState() => _GamePageState();
 }
 
-class _SwipeGamePageState extends State<SwipeGamePage> {
-  List<Color> colors = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.yellow,
-    Colors.orange,
-  ];
-  late Color currentColor;
-  late Color targetColor;
-  Random random = Random();
+class _GamePageState extends State<GamePage> {
+  static const int gameTime = 30; // Game duration in seconds
+  static const double targetSize = 50.0;
+  static const double targetSpeed = 150.0;
+  static const int maxTaps = 10; // Maximum taps allowed
+  static const String gameOverText = 'Game Over';
+  static const String tapToRestartText = 'Tap to Restart';
+
+  late Timer _timer;
+  int _timeRemaining = gameTime;
+  int _tapsRemaining = maxTaps;
+  bool _gameOver = false;
+  double _targetX = 0.0;
+  double _targetY = 0.0;
+  Random _random = Random();
+  late Size _screenSize;
+  int _score = 0;
 
   @override
   void initState() {
     super.initState();
-    currentColor = colors[random.nextInt(colors.length)];
-    targetColor = colors[random.nextInt(colors.length)];
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _screenSize = MediaQuery.of(context).size;
+      _startGame();
+    });
   }
 
-  void changeColor() {
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _startGame() {
+    _gameOver = false;
+    _timeRemaining = gameTime;
+    _tapsRemaining = maxTaps;
+    _score = 0;
+    _resetTargetPosition();
+
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_timeRemaining > 0) {
+          _timeRemaining--;
+        } else {
+          _endGame();
+        }
+      });
+    });
+  }
+
+  void _endGame() {
+    _timer.cancel();
+    _gameOver = true;
+  }
+
+  void _resetTargetPosition() {
+    _targetX = _random.nextDouble() * (_screenSize.width - targetSize);
+    _targetY = _random.nextDouble() * (_screenSize.height - targetSize);
+  }
+
+  void _handleTap() {
+    if (!_gameOver && _tapsRemaining > 0) {
+      setState(() {
+        _score++;
+        _tapsRemaining--;
+        _resetTargetPosition();
+        if (_tapsRemaining == 0) {
+          _endGame();
+        }
+      });
+    }
+  }
+
+  void _restartGame() {
     setState(() {
-      currentColor = colors[random.nextInt(colors.length)];
-      targetColor = colors[random.nextInt(colors.length)];
+      _startGame();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Swipe Game'),
-      ),
-      body: GestureDetector(
-        onPanUpdate: (details) {
-          if (details.delta.dx > 0) {
-            // Swipe right
-            if (currentColor == targetColor) {
-              // Color matched
-              showDialog(
-                context: context,
-                builder: (_) => AlertDialog(
-                  title: Text('Congratulations!'),
-                  content: Text('Color matched!'),
-                  actions: [
-                    TextButton(
-                      child: Text('OK'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        changeColor();
-                      },
+    return GestureDetector(
+      onTap: _gameOver ? _restartGame : _handleTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Tap the Target'),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Time: $_timeRemaining s',
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Taps Remaining: $_tapsRemaining',
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(height: 16),
+                _gameOver
+                    ? Column(
+                  children: [
+                    Text(
+                      gameOverText,
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      tapToRestartText,
+                      style: TextStyle(fontSize: 24),
                     ),
                   ],
+                )
+                    : Container(
+                  width: targetSize,
+                  height: targetSize,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  margin: EdgeInsets.only(
+                    top: _targetY,
+                    left: _targetX,
+                  ),
                 ),
-              );
-            } else {
-              // Color not matched
-              showDialog(
-                context: context,
-                builder: (_) => AlertDialog(
-                  title: Text('Oops!'),
-                  content: Text('Color not matched!'),
-                  actions: [
-                    TextButton(
-                      child: Text('OK'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        changeColor();
-                      },
-                    ),
-                  ],
+                SizedBox(height: 16),
+                Text(
+                  'Score: $_score',
+                  style: TextStyle(fontSize: 24),
                 ),
-              );
-            }
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Swipe to match the color',
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(height: 30),
-              Container(
-                height: 150,
-                width: 150,
-                color: currentColor,
-              ),
-              SizedBox(height: 30),
-              Text(
-                'Swipe right when the color matches',
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: 50,
-                width: 50,
-                color: targetColor,
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
